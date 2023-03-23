6ae0b5c6b2e20855575fd9c68646110a7f1c73fd
Marcela Insegna <marcelainsegna@gmail.com>
2023-03-22 22:39:13 -0300

    añadiendo mis primeros archivos en git

---
+++ scriptEjer.js
@@ -0,0 +1,97 @@
+// /* practica 2 */
+
+// /* ejercicio 1 */
+// for(i=1; i<=10; i++) {
+//     console.log(i)
+// }
+
+// /* ejercicio 2 */
+
+// for(i=2; i<=20;i++){
+//     if (i%2==0)
+//     console.log (i)
+// }
+
+// /* ejercicio  */
+
+// for(i=1; i<=19;i++){
+//     if (i % 2 !== 0)
+//     console.log (i)
+// }
+
+// /* ejercicio 4 */
+//  for(i=10; i>0; i--) {
+//     console.log(i)
+//  }
+
+//  /* ejercicio 5 */
+// for(i=1; i<=10; i++) {
+//     if (i==5)
+//     break;
+//     console.log(i)
+// }
+
+// /* ejercicio 6 */
+// for(i=1; i<=10; i++) {
+//     if(i==5)
+//     continue;
+//     console.log(i)
+// }
+
+// /* ejercicio 7 y 8 */
+// const decirNombre=(nombre) => {
+//     alert("hola " + nombre);
+// }
+// decirNombre("Marcela")
+
+// const decirNombre2=(nombre)=>{
+//     return "hola " + nombre 
+// }
+// alert(decirNombre2("Marcela"))
+
+// /* ejercicio 9 */
+// const sumar=(a,b)=>{
+//     return a+b
+// }
+// console.log (sumar(2,4))
+
+// /*ejercicio 10*/
+// const restar =(a,b) =>{
+//     return a-b
+// }
+// console.log (restar(8,5))
+
+// /* ejercicio 11 */
+// const calcular = (operacion,a,b) =>{
+//     if (operacion=="+") {
+//         return a+b
+//     }
+//     if (operacion=="-") {
+//         return a-b
+//     }
+// }
+// console.log(calcular("-",7,2))
+  
+// /* ejercicio 12 */
+// const contarHasta=(numero) =>{
+//     for (let i=1; i<=numero; i++)
+//     console.log (i)
+// }
+// contarHasta(8)
+
+/*ejercicio 13 */  
+
+operacion = prompt ("ingrese la operacion (+ -)")
+a= Number(prompt("ingrese un valor"))
+b= Number (prompt("ingrese otro valor"))
+
+    const suma = (a,b)
+        suma= a+b
+
+console.log(suma)
+    
+    
+   
+
+    
+   
